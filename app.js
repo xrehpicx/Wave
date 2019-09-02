9acefd2660bde15e0711a6277e9a2816851dba42
Raj <raj.fps2000@gmail.com>
2019-09-02 16:05:31 +0530

    move the box

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 const modelParams = {
-    flipHorizontal: true, 
-    imageScaleFactor: 0.8,
-    maxNumBoxes: 2,       
-    iouThreshold: 0.5,    
-    scoreThreshold: 0.79, 
+    flipHorizontal: true,
+    imageScaleFactor: 0.7,
+    maxNumBoxes: 20,
+    iouThreshold: 0.5,
+    scoreThreshold: 0.7,
 }
 
 const video = document.querySelector('#video');
 const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
-let model;
+var theBox = document.querySelector('.element');
+let model = null;
+let counter = 0;
+let valuelist = {
+    x: 0,
+    y: 0,
+};
+//context.fillRect(10, 10, 150, 75 );
 
 handTrack.startVideo(video)
     .then(status => {
@@ -18,6 +25,7 @@ handTrack.startVideo(video)
             navigator.getUserMedia({ video: {} }, stream => {
                 video.srcObjecy = stream;
                 runDetection();
+                //setInterval(runDetection, 100);
             },
                 err => console.log(err)
             );
@@ -27,8 +35,28 @@ handTrack.startVideo(video)
 const runDetection = () => {
     model.detect(video)
         .then(predictions => {
-            console.log(predictions);
-            model.renderPredictions(predictions, canvas, context, video);
+            /* model.renderPredictions(predictions, canvas, context, video); */
+            if (predictions.length > 0) {
+                let x = predictions[0].bbox[0] * 2 + 'px';
+                let y = predictions[0].bbox[1] + 'px';
+                theBox.style.top = y;
+                theBox.style.left = x;
+                console.log(x, y);
+                //console.log(predictions[0].bbox[0] / 2, predictions[0].bbox[1] / 2);
+                /* console.log(valuelist.x,valuelist.y);
+                if (counter < 10) {
+                    counter += 1;
+                    valuelist.x = (predictions[0].bbox[0] * 2) / counter;
+                    valuelist.y = predictions[0].bbox[1] / counter;
+                }
+                else{
+                    theBox.style.top = valuelist.y + 'px';
+                    theBox.style.left = valuelist.x + 'px';
+                    console.log('SET');
+                    counter=0;
+                } */
+                /* context.fillRect(predictions[0].bbox[0], predictions[0].bbox[1],10,10); */
+            }
             requestAnimationFrame(runDetection);
         });
 }
